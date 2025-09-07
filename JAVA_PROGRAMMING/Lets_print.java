32047cf3d47731508665147056b143f15f53e51e
Ammi2019 <mufwambiforb@gmail.com>
2025-09-07 07:22:54 +0200

    DMIU Java programming

---
+++ JAVA_PROGRAMMING/Lets_print.java
@@ -0,0 +1,11 @@
+public class Lets_print
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Three...\n");
+		System.out.print("Two...\n");
+		System.out.print("One...");
+		System.out.print("Zero...");
+		System.out.println("Liftoff!");
+		System.out.println("Houston, we have a problem");	}
+}
---
+++ JAVA_PROGRAMMING/My_Info.java
@@ -0,0 +1,7 @@
+public class My_Info
+{
+	public static void main(String[] args)
+	{	
+		System.out.println (" Name: Forb Mufwambi,\n Date of Birth: 29th April 1994,\n My Hobbies: movies, singing, cooking.\n Favorite book: Sherperds Rod.\n Favorite Movie: Fast and Furious");
+	}
+}
